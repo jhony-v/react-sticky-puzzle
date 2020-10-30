df03e5e7d6bc6ce9dfeaf053a5bbe7120ee50b5e
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-10-30 18:16:06 -0500

    feat: Add new feature to scrolling puzzle in scrolling element

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import ReactStickyPuzzle, { StickyPuzzleItem } from "./lib/ReactStickyPuzzle";
 const App = () => {
   return (
     <div>
-      <ReactStickyPuzzle>
+      <Box i={4} />
+      <div className="container-sticky" style={{
+        width:"300px",
+        height:"400px",
+        overflowY: "scroll",
+        background:"rgba(0,0,100,.1)",
+        margin:"auto"
+      }}>
+      <ReactStickyPuzzle isInElement>
         <Box i={3} />
-        <StickyPuzzleItem styleSticky={{
-          background : "gray"
-        }}>
+        <StickyPuzzleItem>
           <div> title 1</div>
         </StickyPuzzleItem>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, ex maiores impedit sed porro natus animi in dolorum, eaque ipsa officiis voluptatum blanditiis? Quas totam fugiat dolorem expedita vero. Illum!
@@ -29,6 +35,26 @@ const App = () => {
         </StickyPuzzleItem>
         <Box i={100} />
       </ReactStickyPuzzle>
+      </div>
+        
+      <ReactStickyPuzzle>
+        <Box i={3} />
+        <StickyPuzzleItem styleSticky={{
+          background : "gray"
+        }}>
+          <div> title 1</div>
+        </StickyPuzzleItem>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, ex maiores impedit sed porro natus animi in dolorum, eaque ipsa officiis voluptatum blanditiis? Quas totam fugiat dolorem expedita vero. Illum!
+        <Box i={20} />
+        <StickyPuzzleItem>
+          <div> title 3</div>
+        </StickyPuzzleItem>
+        <StickyPuzzleItem>
+          <div style={{background:"lime"}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel tempore facilis dolore molestiae adipisci, dolorem, nostrum veritatis nisi laudantium ipsam fuga, placeat asperiores officiis! Quae incidunt porro a in!</div>
+        </StickyPuzzleItem>
+        <Box i={100} />
+      </ReactStickyPuzzle>
+
     </div>
   );
 };
--- src/lib/ReactStickyPuzzle/components/Container.jsx
+++ src/lib/ReactStickyPuzzle/components/Container.jsx
@@ -1,71 +1,144 @@
-import React, { useEffect, useReducer, useRef } from "react";
+import React, { useCallback, useEffect, useReducer, useRef } from "react";
 import "../css/index.css";
 import Item from "./Item";
 import { sumMatrixToSelectedIndex, transformMapToArray } from "../utils/stickyPuzzleUtils";
 import { actionSetStickyItem , initialState, reducer } from "./reducer";
 
-const Container = ({ children, onFixed }) => {  
+const Container = ({ children, onFixed, isInElement }) => {  
   const [state, action] = useReducer(reducer, initialState);
   const refItems = useRef(new Map()).current;
+  const refContainerElement = useRef(null);
 
+  // Get elements with data in array of objects
+  const getArrayElements = () => transformMapToArray(refItems,({key,value}) => ({
+    key,
+    isSticky : true, 
+    element : value,
+    bounding: value.getBoundingClientRect(),
+  }));
+
+  // Get data elements and length of elements
+  const propsElements = () => {
+    const elements = getArrayElements();
+    const lengthElements = elements.length;
+    return {
+      elements,
+      lengthElements
+    }
+  }
+
+  // Emit and destroy scroll event 
+  const onScrollEvent = (element,onScrollCallback) => {
+    element.addEventListener("scroll",onScrollCallback);
+    return () => element.removeEventListener("scroll",onScrollCallback);
+  }
+
+  // Dispatch event action to set sticky item
+  const setActionSticky = (element,{key,isSticky}) => {
+    if(element.isSticky === isSticky){
+      element.isSticky = !isSticky;
+      action(actionSetStickyItem({
+        key,
+        value : {
+          isSticky
+        }
+      }))
+    }
+  }
+
+  const dispatchOnFixedIsSticky = (element) => {
+    if(element.isSticky) {
+      onFixed && onFixed();
+    }
+  }
+
+  // Get the sum total of the height of the elements up to an index  
+  const getSumTotalOfHeightElementsLimit = (elements) => {
+    const el = elements;
+    return (index) => sumMatrixToSelectedIndex(el,index,item => item.bounding.height);
+  }
+  
+  // Scrolling element
   useEffect(() => {
-    const elements = transformMapToArray(refItems,({key,value}) => ({
-      key,
-      isSticky : true, 
-      element : value,
-      value: value.getBoundingClientRect(),
-    }));
-    const sizeElements = elements.length;
+    const {  elements, lengthElements } = propsElements();
+    const sumTotalHeight = getSumTotalOfHeightElementsLimit(elements);
+
+    const onScroll = (e) => {
+      const { scrollTop, offsetTop, offsetLeft, clientWidth } = e.target;
+      for(let i = 0; i < lengthElements; i++) {
+        let { key, bounding, element } = elements[i];
+        let spaceFromTop = sumTotalHeight(i);
+        const boundingMarginTop = (offsetTop - window.scrollY) + spaceFromTop;
+        const isSticky = scrollTop > ( bounding.top - (elements[i - 1]?.bounding?.height || 0) - boundingMarginTop );
+        if (isSticky) {
+          let topPosition = boundingMarginTop - window.scrollY;
+          element.style.position = "fixed";
+          element.style.width = clientWidth + "px";
+          element.style.top = topPosition + "px";
+          element.style.left = offsetLeft + "px";
+          dispatchOnFixedIsSticky(elements[i]);
+        }
+        else {
+          element.style.removeProperty("position");
+        }
+        setActionSticky(elements[i],{ key, isSticky });
+      }
+    }
+    if(isInElement) {
+      refContainerElement.current = refContainerElement.current.parentNode;
+      return onScrollEvent(refContainerElement.current,onScroll);
+    }
+    // eslint-disable-next-line
+  },[isInElement])
+
+
+  // Scrolling in screen
+  const scrollPage = useCallback(() => {
+    const {  elements, lengthElements } = propsElements();
+    const sumTotalHeight = getSumTotalOfHeightElementsLimit(elements);
 
     const onScroll = () => {
-        for(let i = 0; i < sizeElements; i++) {
-          let { key, value, element } = elements[i];
-          let spaceFromTop = sumMatrixToSelectedIndex(elements,i,item => item.value.height);
-          let isSticky = window.scrollY > (value.top - (elements[i - 1]?.value?.height || 0));
-          let verifiyStickyChange = () => {
-            if(elements[i].isSticky === isSticky){
-              elements[i].isSticky = !isSticky;
-              action(actionSetStickyItem({
-                key,
-                value : {
-                  isSticky
-                }
-              }))
-              }
-          }
+        for(let i = 0; i < lengthElements; i++) {
+          let { key, bounding, element } = elements[i];
+          let spaceFromTop = sumTotalHeight(i);
+          let isSticky = window.scrollY > (bounding.top - (elements[i - 1]?.bounding?.height || 0));
           if(isSticky) {
             element.classList.add("sticky__item");
             element.style.marginTop = spaceFromTop + "px";
-            element.nextSibling.style.height = value.height + "px";
-            if(elements[i].isSticky) {
-              onFixed && onFixed();
-            }
+            element.nextSibling.style.height = bounding.height + "px";
+            dispatchOnFixedIsSticky(elements[i]);
           } 
           else {
             element.classList.remove("sticky__item");
             element.nextSibling.style.height = "0px";
           }
-          verifiyStickyChange();
+          setActionSticky(elements[i],{ key, isSticky });
         }
     }
-    window.addEventListener("scroll",onScroll);
-    return () => window.removeEventListener("scroll",onScroll);
+    return onScrollEvent(window,onScroll);
     // eslint-disable-next-line
-  },[]);
+  },[])
 
-  return React.Children.map(children, (e, i) => {
+  // scrolling by default of type page
+  useEffect(() => {
+    !isInElement && scrollPage();
+  },[scrollPage,isInElement]);
+
+  return(
+    <div ref={refContainerElement}>
+    {React.Children.map(children, (e, i) => {
       if(e.type === Item) {
         const { isSticky } = state.items[i] || {};
         const ElementCloned = React.cloneElement(e,{
-            refItem : node => {
-              refItems.set(i,node);
-            },
+            refItem : node => refItems.set(i,node) ,
             isSticky
         })
         return ElementCloned;
       }
       return e;
-  });
+    })}
+    </div>
+  )
 };
 
 export default Container;
